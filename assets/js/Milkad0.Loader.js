0b3ceec40a4db31864cd4cf10060a7c7385282af
Milkad0 <44178372+Milkad0@users.noreply.github.com>
2022-09-09 13:25:20 +0200

    turn on

--- assets/js/Milkad0.Loader.js
+++ assets/js/Milkad0.Loader.js
@@ -75,11 +75,13 @@ var Loader = (function($) {
     }
 
     function zeroPad(num, places) {
+
         var zero = places - num.toString().length + 1;
         return Array(+(zero > 0 && zero)).join("0") + num;
     }
 
     return {
+
         preload: preload
     };
 
